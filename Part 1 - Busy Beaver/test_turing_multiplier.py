3f5b656316bda8c3917539245145ce742d4fb4e1
Amirreza Moghimi <Amir06mgh@gmail.com>
2026-06-30 10:49:19 +0330

    Add turing unary multiplier

--- Part 1 - Busy Beaver/test_turing_multiplier.py
+++ Part 1 - Busy Beaver/test_turing_multiplier.py
@@ -1,33 +1,55 @@
-# -*- coding: utf-8 -*-
 from turing_machine import TuringMachine
 from test_turing_machine_example1 import print_states
 
-#create the Turing machine
 transitions = {
-        # TODO: Part II b) - Write your transition rules here as entries to a Python dictionary
-        # For example, the key will be a pair (state, character)
-        # The value will be the triple (next state, character to write, move head L or R)
-        # such as ('q0', '1'): ('q1', '0', 'R'), which says if current state is q0 and 1 encountered
-        # then transition to state q1, write a 0 and move head right.
+    ('q0', '1'): ('q1', 'X', 'R'),
+    ('q0', '0'): ('q5', 'M', 'L'),
+
+    ('q1', '1'): ('q1', '1', 'R'),
+    ('q1', '0'): ('q2', '0', 'R'),
+
+    ('q2', '1'): ('q3', 'Y', 'R'),
+    ('q2', 'Y'): ('q2', 'Y', 'R'),
+    ('q2', 'Z'): ('q2', 'Z', 'R'),
+    ('q2', ''): ('q7', '', 'L'),
+
+    ('q3', '1'): ('q3', '1', 'R'),
+    ('q3', 'Z'): ('q3', 'Z', 'R'),
+    ('q3', ''): ('q4', 'Z', 'L'),
+
+    ('q4', 'Z'): ('q4', 'Z', 'L'),
+    ('q4', '1'): ('q4', '1', 'L'),
+    ('q4', 'Y'): ('q2', 'Y', 'R'),
+
+    ('q5', 'X'): ('q5', '', 'L'),
+    ('q5', ''): ('q8', '', 'R'),
+
+    ('q6', '1'): ('q6', '1', 'L'),
+    ('q6', 'X'): ('q0', 'X', 'R'),
+
+    ('q7', 'Z'): ('q7', 'Z', 'L'),
+    ('q7', 'Y'): ('q7', '1', 'L'),
+    ('q7', '0'): ('q6', '0', 'L'),
+    
+    ('q8', ''): ('q8', '', 'R'),
+    ('q8', 'M'): ('q9', '', 'R'),
+    
+    ('q9', '1'): ('q9', '', 'R'),
+    ('q9', 'Z'): ('q9', '1', 'R'),
+    ('q9', ''): ('qa', '', 'L'),
 }
+
 if __name__ == "__main__":
     print_states(transitions)
     machine = TuringMachine(transitions)
 
     def run(input_):
         w = input_
-        print("Input:",w)
+        print("Input:", w)
         print("Accepted" if machine.accepts(w) else "Rejected")
-        machine.debug(w, step_limit=1000)
-
+        machine.debug(w, step_limit=2500)
         print()
 
-    # SHOULD ACCEPT
-    run("110111")
-    # outputs 111111
-
-    # SHOULD ACCEPT
-    run("11101111")
-    # outputs 111111111111
-
-    run("01111")
+    run("110111") #Accept 111111
+    run("11101111") #Accept 111111111111
+    run("01111") #Accept 0
